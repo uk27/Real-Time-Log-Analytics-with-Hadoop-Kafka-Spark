7cbd148b74bae949ffca5264517f45c70bd9a975
Utkarsh <kajar001@umn.edu>
2017-06-10 15:08:27 -0500

    Cleaning up of HdfsProducer

--- src/hadoopTest/HdfsProducer.java
+++ src/hadoopTest/HdfsProducer.java
@@ -19,87 +19,95 @@ import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
+
+
 public class HdfsProducer {
-
-	public static void readFromHdfs(Producer<String, String> producer, String topicName, String fileName) {
-
-		try{
-			//1. Get the instance of Configuration
-			Configuration configuration = new Configuration();
-			//2. URI of the file to be read
-			//URI uri = new URI("hdfs://0.0.0.0:8022/data/apache-access-log.txt");
-			URI uri = new URI(fileName);
-			//3. Get the instance of the HDFS 
-			FileSystem hdfs = FileSystem.get(uri, configuration);
-			Path pt = new Path(uri);
-			BufferedReader br=new BufferedReader(new InputStreamReader(hdfs.open(pt)));
-			String line;
-			
-			line=br.readLine();
-			int count = 1;
-			//while (line != null){
-			while (line != null && count <10){
-				
-				System.out.println("Sending batch" + count);
-				producer.send(new ProducerRecord<String, String>(topicName, new String(line)));
-				line=br.readLine();
-				count = count+1;				
-			}
-			
-			producer.close();
-		}catch(Exception e){
-			System.out.println(e);
-		}
-	}
-	
-	public static Producer<String, String> getProducer(String brokers, String topicName)throws Exception{
-		
-		// create instance for properties to access producer configs   
-				Properties props = new Properties();
-
-				//Assign localhost id
-				props.put("bootstrap.servers", brokers);
-				
-				props.put("auto.create.topics.enable", "true");
-
-				//Set acknowledgements for producer requests.      
-				props.put("acks", "all");
-
-				//If the request fails, the producer can automatically retry,
-				props.put("retries", 0);
-
-				//Specify buffer size in config
-				props.put("batch.size", 16384);
-
-				//Reduce the no of requests less than 0   
-				props.put("linger.ms", 1);
-
-				//The buffer.memory controls the total amount of memory available to the producer for buffering.   
-				props.put("buffer.memory", 33554432);
-
-				props.put("key.serializer", 
-						"org.apache.kafka.common.serialization.StringSerializer");
-
-				props.put("value.serializer", 
-						"org.apache.kafka.common.serialization.StringSerializer");
-				
-				props.put("topic.metadata.refresh.interval.ms", "10");
-
-				Producer<String, String> producer = new KafkaProducer
-						<String, String>(props);
-				
-				return producer;
-	}
-
-	
-	public static void init(String brokers, String topicName, String fileName){
-		try {
-			readFromHdfs(getProducer(brokers, topicName), topicName, fileName);
-			System.out.println("Producer exited normally for "+fileName);
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.out.println("Exception while producing messages for "+fileName+"\n" +e);
-		}
-	}
+    
+    
+    private String brokers;
+    private String topicName;
+    private String fileName;
+    private Producer<String, String> producer;
+    
+    
+    public HdfsProducer(String brokers, String topicName, String fileName){
+        
+        this.topicName = topicName;
+        this.brokers = brokers;
+        this.fileName = fileName;
+        this.producer = getProducer();
+    }
+    
+    public void produce(){
+        
+        try{
+            //1. Get the instance of Configuration
+            Configuration configuration = new Configuration();
+            //2. URI of the file to be read
+            URI uri = new URI(fileName);
+            //3. Get the instance of the HDFS
+            FileSystem hdfs = FileSystem.get(uri, configuration);
+            Path pt = new Path(uri);
+            BufferedReader br=new BufferedReader(new InputStreamReader(hdfs.open(pt)));
+            String line;
+            
+            line=br.readLine();
+            int count = 1;
+            //while (line != null){
+            while (line != null && count <10){
+                
+                System.out.println("Sending batch" + count);
+                producer.send(new ProducerRecord<String, String>(topicName, new String(line)));
+                line=br.readLine();
+                count = count+1;
+            }
+            
+            producer.close();
+            System.out.println("Producer exited successfully for " + fileName);
+            
+        }catch(Exception e){
+            
+            System.out.println("Exception while producing for " + fileName);
+            System.out.println(e);
+        }
+    }
+    
+    private Producer<String, String> getProducer(){
+        // create instance for properties to access producer configs
+        Properties props = new Properties();
+        
+        //Assign localhost id
+        props.put("bootstrap.servers", brokers);
+        
+        props.put("auto.create.topics.enable", "true");
+        
+        //Set acknowledgements for producer requests.
+        props.put("acks", "all");
+        
+        //If the request fails, the producer can automatically retry,
+        props.put("retries", 0);
+        
+        //Specify buffer size in config
+        props.put("batch.size", 16384);
+        
+        //Reduce the no of requests less than 0
+        props.put("linger.ms", 1);
+        
+        //The buffer.memory controls the total amount of memory available to the producer for buffering.
+        props.put("buffer.memory", 33554432);
+        
+        props.put("key.serializer", 
+                  "org.apache.kafka.common.serialization.StringSerializer");
+        
+        props.put("value.serializer", 
+                  "org.apache.kafka.common.serialization.StringSerializer");
+        
+        props.put("topic.metadata.refresh.interval.ms", "10");
+        
+        Producer<String, String> producer = new KafkaProducer
+        <String, String>(props);
+        
+        return producer;
+    }
+    
 }
---
+++ src/hadoopTest/ProducerDriver.java
@@ -0,0 +1,87 @@
+package hadoopTest.hadoopTest;
+//import util.properties packages
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+//import Hadoop Packages
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+
+public class ProducerDriver {
+    
+    public static Set<String> fileSet = new HashSet<String>();
+    public static Map<String, HashSet<String>> fileMap = new HashMap<String, HashSet<String>>();
+    public static void main(String args[]){
+        
+        // Check arguments length value
+        if(args.length < 2){
+            System.out.println("Usage: HdfsProducer <brokers> <hdfs directory>");
+            return;
+        }
+        
+        
+        while(true){
+            
+            try{
+                
+                Configuration configuration = new Configuration();
+                
+                URI uri = new URI("hdfs://0.0.0.0:8022");
+                
+                FileSystem fs = FileSystem.get(uri, configuration);
+                
+                RemoteIterator<LocatedFileStatus> fileStatusListIterator = fs.listFiles(
+                                                                                        new Path(args[1]), true);
+                
+                while(fileStatusListIterator.hasNext()){
+                    
+                    String fileName = fileStatusListIterator.next().getPath().toString();
+                    String segments[] = fileName.split("/");
+                    String topic = segments[segments.length-2];
+                    
+                    if(!fileMap.containsKey(topic) || !(fileMap.get(topic).contains(fileName))){
+                        
+                        HashSet<String> temp = fileMap.get(topic);
+                        if(temp == null)
+                            temp = new HashSet<String>();
+                        
+                        temp.add(fileName);
+                        fileMap.put(topic, temp);
+                        
+                        //Create a producer thread
+                        System.out.println("Creating a new thread for topic "+topic);
+                        System.out.println("Filename "+fileName);
+                        try {
+                            
+                            Runnable producerThread = new Runnable() {
+                                public void run() {
+                                    
+                                    HdfsProducer hdfsProducer = new HdfsProducer(args[0], topic, fileName);
+                                    hdfsProducer.produce();
+                                }
+                            };
+                            
+                            new Thread(producerThread).start();
+                        } catch (Exception x) {
+                            x.printStackTrace();
+                        }
+                    }
+                    
+                }
+                //Poll for new files every 3 seconds
+                Thread.sleep(3000);
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+                System.exit(1);
+            }
+        }
+    }
+}
