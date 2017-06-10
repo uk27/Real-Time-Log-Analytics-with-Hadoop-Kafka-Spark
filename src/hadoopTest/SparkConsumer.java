5b29df77e1590ace273e2fc7ab83e7fa25e73ce3
Utkarsh <kajar001@umn.edu>
2017-06-10 15:41:41 -0500

    More Cleanup of consumer

--- src/hadoopTest/SparkConsumer.java
+++ src/hadoopTest/SparkConsumer.java
@@ -1,40 +1,26 @@
 package hadoopTest.hadoopTest;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 import java.util.Date;
-import java.util.concurrent.*;
-import java.util.List;
 import java.util.regex.Pattern;
 import java.text.*;
 
 import scala.Tuple2;
-import kafka.serializer.StringDecoder;
 
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.*;
 import org.apache.spark.streaming.api.java.*;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.streaming.kafka010.*;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.TopicPartition;
-//import org.apache.spark.streaming.kafka._;
-//import org.apache.spark.sql.SQLContext;
 import org.apache.spark.streaming.Durations;
 //import org.apache.spark.sql.api.java.JavaSQLContext;
-import org.codehaus.jettison.mapped.Configuration;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-import com.sun.org.apache.regexp.internal.RE;
-
-//import com.sun.tools.javac.util.List;
 
 public class SparkConsumer {
 	
@@ -79,25 +65,25 @@ public class SparkConsumer {
 	    //kafkaParams.put("max.poll.interval.ms", Integer.toString(Integer.MAX_VALUE));
 	    
 	    final JavaInputDStream<ConsumerRecord<String, String>> messages =
-	    		  KafkaUtils.createDirectStream(
-	    		    jssc,
-	    		    LocationStrategies.PreferBrokers(),
-	    		    ConsumerStrategies.SubscribePattern(p, kafkaParams)
-	    		  );
+	    			KafkaUtils.createDirectStream(
+	    			jssc,
+	    			LocationStrategies.PreferBrokers(),
+	    			ConsumerStrategies.SubscribePattern(p, kafkaParams)
+	    		);
 	    
 	    JavaPairDStream<String, String> key_value = messages.mapToPair(
-	    		  new PairFunction<ConsumerRecord<String, String>, String, String>() {
-	    		    @Override
-	    		    public Tuple2<String, String> call(ConsumerRecord<String, String> record) throws Exception {
-	    		      return new Tuple2<>(record.key(), record.value());
+	    			new PairFunction<ConsumerRecord<String, String>, String, String>() {
+	    			@Override
+	    			public Tuple2<String, String> call(ConsumerRecord<String, String> record) throws Exception {
+	    				return new Tuple2<>(record.key(), record.value());
 	    		    }
 	    		  });
 
 
-	    key_value.foreachRDD(rdd -> {
-	    	
-	    	long numHits = rdd.count();
-	    	System.out.println("Number of partitions fetched: " + rdd.partitions().size());
+		key_value.foreachRDD(rdd -> {
+
+			long numHits = rdd.count();
+			System.out.println("Number of partitions fetched: " + rdd.partitions().size());
             
 	    	if(numHits < 1500)
 	    		System.out.println("No new data fetched in last 30 sec");
@@ -106,16 +92,16 @@ public class SparkConsumer {
 	    	else{
                     //Convert to java log object
 	    			JavaRDD<ApacheAccessLog> logs =
-                                rdd.map(x-> x._2)
-                                .map(ApacheAccessLog::parseFromLogLine)
-                                .cache();
+	    					rdd.map(x-> x._2)
+	    					.map(ApacheAccessLog::parseFromLogLine)
+	    					.cache();
 	        
 	    			//Find the bot ip addresses
-	    			JavaRDD<String> iprdd =
-                                logs.mapToPair(ip-> new Tuple2<>(ip.getIpAddress(),1))
-                                .reduceByKey(MyReducerFunc)
-                                .filter(botip-> botip._2 > 50)
-                                .keys();
+	    			JavaRDD<String> iprdd = 
+	    					logs.mapToPair(ip-> new Tuple2<>(ip.getIpAddress(),1))
+	    					.reduceByKey(MyReducerFunc)
+	    					.filter(botip-> botip._2 > 50)
+	    					.keys();
 	    			
 	    			//If we find something, we store it in results dir on hdfs
 	    			long botIpCount = iprdd.count();
@@ -124,13 +110,12 @@ public class SparkConsumer {
                         //Set file configuration for hdfs
 	    				sc.hadoopConfiguration().set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
 	    				sc.hadoopConfiguration().set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
-	    				
+
+	    				//Combine the results and store
 	    				String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-                    
-	    				iprdd.coalesce(1).saveAsTextFile("hdfs://quickstart.cloudera:8020/results/"+timeStamp);
-                        
-                        System.out.println("\n---------"+botIpCount+" Bot Ips were detected---------");
-                        System.out.println("\n---------Please see /results for details---------\n");
+                    	iprdd.coalesce(1).saveAsTextFile("hdfs://quickstart.cloudera:8020/results/"+timeStamp);
+	    				System.out.println("\n---------"+botIpCount+" Bot Ips were detected---------");
+	    				System.out.println("\n---------Please see /results for details---------\n");
 	    			}
 	    				
 	    		}        
